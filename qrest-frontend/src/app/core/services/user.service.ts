c0faea1a009168dd208b696eae77db3bd6edafc5
JoyserB <BR23021@ues.edu.sv>
2025-12-07 10:38:04 -0600

    Fix UserService base URL

--- qrest-frontend/src/app/core/services/user.service.ts
+++ qrest-frontend/src/app/core/services/user.service.ts
@@ -2,10 +2,11 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { UserDTO, UserCreateDTO, UserUpdateDTO, Page } from '../../models/user.model';
+import { environment } from '../../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class UserService {
-  private base = '/api/users';
+  private base = `${environment.apiUrl}/api/users`;
 
   constructor(private http: HttpClient) {}
 
